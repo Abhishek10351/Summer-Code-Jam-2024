5e7a1e6426af66db2bb113ad65551a210bacd7b9
Abhishek Kashyap <abhik5567@gmail.com>
2024-07-19 01:08:01 +0530

    add basic bot runner file

---
+++ main.py
@@ -0,0 +1,62 @@
+import os
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("TOKEN")
+server = os.getenv("SERVER")
+MY_GUILD = discord.Object(id=server)
+
+intents = discord.Intents.all()
+allowed_installs = discord.app_commands.AppInstallationType(
+    guild=discord.Object(id=server))
+
+
+class Bot(commands.Bot):
+    """Bot class."""
+
+    def __init__(self) -> None:
+        """Bot Initialization."""
+        super().__init__(command_prefix="!", case_insensitive=True,
+                         strip_after_prefix=True, intents=intents,
+                         allowed_installs=allowed_installs)
+
+    async def setup_hook(self) -> None:
+        """Setups hook for the bot."""
+        # This copies the global commands over to your guild.
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
+    async def on_ready(self) -> None:
+        """Call when bot is logged in."""
+        print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+        print("------")
+        await bot.change_presence(activity=discord.Game(name="/help"))
+        t = await bot.tree.sync()
+        print(t)
+
+    async def load_extensions() -> None:
+        """Load all extensions in the cogs directory."""
+        extension_path = "cogs"
+        for filename in os.listdir(extension_path):
+            if filename.endswith(".py"):
+                await bot.load_extension(f"{extension_path}.{filename[:-3]}")
+                print("Extension: " + filename + " loaded.")
+
+
+bot = Bot()
+
+
+@bot.tree.command(name="hello")
+async def hello(interaction: discord.Interaction) -> None:
+    """Say hello!."""
+    await interaction.response.send_message(f"Hi, {interaction.user.mention}.. Whatcha doin?")
+
+
+try:
+    bot.run(BOT_TOKEN)
+except KeyboardInterrupt:
+    print("\nKeyboardInterrupt is raised. Exiting.".upper())
