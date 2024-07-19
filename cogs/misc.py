8e51c7884e2331260e952d41bc8e7f6eb2d11699
Abhishek Kashyap <abhik5567@gmail.com>
2024-07-19 22:47:30 +0530

    add a misc cog

---
+++ cogs/misc.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+from discord import app_commands
+
+
+class MiscCommand(commands.Cog):
+    """Misc commands cog."""
+
+    def __init__(self, bot: commands.Bot) -> None:
+        """Initialize MiscCommand cog."""
+        self.bot = bot
+
+    @app_commands.command(name="ping")
+    async def ping(self, interaction: discord.Interaction) -> None:
+        """Ping the bot."""
+        await interaction.response.send_message(f"Pong! Latency: **{round(self.bot.latency * 1000)}ms**")
+
+
+async def setup(bot: commands.Bot) -> None:
+    """Setups the misc command."""
+    await bot.add_cog(MiscCommand(bot))
--- main.py
+++ main.py
@@ -12,7 +12,8 @@ MY_GUILD = discord.Object(id=server)
 
 intents = discord.Intents.all()
 allowed_installs = discord.app_commands.AppInstallationType(
-    guild=discord.Object(id=server))
+    guild=discord.Object(id=server),
+)
 
 
 class Bot(commands.Bot):
@@ -20,29 +21,30 @@ class Bot(commands.Bot):
 
     def __init__(self) -> None:
         """Bot Initialization."""
-        super().__init__(command_prefix="!", case_insensitive=True,
-                         strip_after_prefix=True, intents=intents,
-                         allowed_installs=allowed_installs)
+        super().__init__(
+            command_prefix="!",
+            case_insensitive=True,
+            strip_after_prefix=True,
+            intents=intents,
+        )
 
     async def setup_hook(self) -> None:
         """Setups hook for the bot."""
         # This copies the global commands over to your guild.
-        self.tree.copy_global_to(guild=MY_GUILD)
         await self.tree.sync(guild=MY_GUILD)
 
     async def on_ready(self) -> None:
         """Call when bot is logged in."""
         print(f"Logged in as {bot.user} (ID: {bot.user.id})")
         print("------")
+        await self.load_extensions()
         await bot.change_presence(activity=discord.Game(name="/help"))
-        t = await bot.tree.sync()
-        print(t)
 
-    async def load_extensions() -> None:
+    async def load_extensions(self) -> None:
         """Load all extensions in the cogs directory."""
         extension_path = "cogs"
         for filename in os.listdir(extension_path):
-            if filename.endswith(".py"):
+            if filename.endswith(".py") and filename != "__init__.py":
                 await bot.load_extension(f"{extension_path}.{filename[:-3]}")
                 print("Extension: " + filename + " loaded.")
 
@@ -53,7 +55,9 @@ bot = Bot()
 @bot.tree.command(name="hello")
 async def hello(interaction: discord.Interaction) -> None:
     """Say hello!."""
-    await interaction.response.send_message(f"Hi, {interaction.user.mention}.. Whatcha doin?")
+    await interaction.response.send_message(
+        f"Hi, {interaction.user.mention}.. Whatcha doin?",
+    )
 
 
 try:
