3864229fe82bfe178b0690e4d340d57e0b251dc7
Abhishek Kashyap <abhik5567@gmail.com>
2024-07-27 19:48:03 +0530

    save quiz tokens in mongo

--- utils/database.py
+++ utils/database.py
@@ -1,6 +1,6 @@
 import logging
 import os
-from dotenv import load_dotenv
+
 import motor.motor_asyncio
 
 logger = logging.getLogger("db")
@@ -15,6 +15,7 @@ class Database:
         self.db = self.client["bot-data"]
         self.scores = self.db["scores"]
         self.commands_cache = self.db["commands_cache"]
+        self.quiz_tokens = self.db["quiz_tokens"]
 
         logger.info("Connected to MongoDB database.")
 
@@ -26,13 +27,15 @@ class Database:
     async def set_score(self, user_id: int, score: int) -> None:
         """Set the score of a user."""
         await self.scores.update_one(
-            {"user_id": user_id}, {"$set": {"score": score}}, upsert=True
+            {"user_id": user_id},
+            {"$set": {"score": score}},
+            upsert=True,
         )
 
     async def command_is_active(self, command_name: str, channel_id: int) -> bool:
         """Check if a command is active."""
         command = await self.commands_cache.find_one(
-            {"command_name": command_name, "channel_id": channel_id}
+            {"command_name": command_name, "channel_id": channel_id},
         )
         return command["active"] if command else False
 
@@ -55,6 +58,18 @@ class Database:
     async def clear_command_cache(self) -> None:
         """Clear the command cache."""
         await self.commands_cache.delete_many({})
+
+    async def get_token(self, channel_id: int) -> dict:
+        """Return all currently tokens."""
+        return await self.quiz_tokens.find_one({"channel_id": channel_id})
+
+    async def change_token(self, channel_id: int, token: str) -> None:
+        """Change the token of a channel."""
+        await self.quiz_tokens.update_one(
+            {"channel_id": channel_id},
+            {"$set": {"token": token}},
+            upsert=True,
+        )
 
     async def close(self) -> None:
         """Close the database connection."""
